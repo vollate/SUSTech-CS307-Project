70d22734eb95385543e0fb1e602e86a2397bebae
Vollate <uint44t@gmail.com>
2023-04-29 14:22:20 +0800

    feat(init vue web ui): framework complete basic framework

--- Project2/the-end-bbs/src/Main.vue
+++ Project2/the-end-bbs/src/Main.vue
@@ -1,25 +1,33 @@
 <template>
     <NavBar v-bind="NavBarParameters" @change-nav="changeNav"/>
-    <MainPage />
+    <Home v-if="enableHome"/>
+    <Post v-if="enablePost"/>
+    <Category v-if="enableCategory"/>
+    <User v-if="enableUser"/>
 </template>
 
 <script setup>
-import {onBeforeMount} from "vue";
-import NavBar from "./apps/NavBar.vue";
-import MainPage from "./apps/Home.vue";
+import {computed, reactive} from "vue";
+import NavBar from "./componment/NavBar.vue";
+import Home from "./componment/Home.vue";
+import Post from "./componment/Post.vue";
+import Category from "./componment/Category.vue";
+import User from "./componment/User.vue";
 
-let NavBarParameters = {
-    selectedBar: 'Home',
-    navBarInfo: [['#', 'Home'], ['#', 'Posts'], ['#', 'Category'], ['#', 'User']]
-
-}
-onBeforeMount(() => {
+const NavBarParameters = reactive({
+    curSelected: 'Home',
+    navBarInfo: [['Home'], ['Post'], ['Category'], ['User']]
 })
+const enableHome = computed(() => NavBarParameters.curSelected === 'Home');
+const enablePost = computed(() => NavBarParameters.curSelected === 'Post');
+const enableCategory = computed(() => NavBarParameters.curSelected === 'Category');
+const enableUser = computed(() => NavBarParameters.curSelected === 'User');
 
 function changeNav(clickedName) {
-    NavBarParameters.selectedBar=clickedName;
-
+    NavBarParameters.curSelected = clickedName;
 }
+
+
 </script>
 
 <style scoped>
---
+++ Project2/the-end-bbs/src/componment/Home.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Post from "./Post.vue";
+</script>
+
+<template>
+    <div class="container text-center" id="main-list">
+        <div class="mb-5 h-auto"></div>
+        <p class="main-title">THE END</p>
+        <p class="sub-title">The end will never end</p>
+        <div class="mb-5 h-auto"></div>
+        <Post/>
+    </div>
+</template>
+
+<style scoped>
+.main-title {
+    font-size: 70pt;
+    color: rgba(0, 44, 169, 0.89);
+}
+
+.sub-title {
+    font-size: 20pt;
+    color: #00865e;
+}
+</style>
