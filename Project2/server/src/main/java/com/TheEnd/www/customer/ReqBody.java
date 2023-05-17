71f685657e8d426d561518fe3c97492731dbd9d8
Vollate <uint44t@gmail.com>
2023-05-17 11:52:04 +0800

    file(add static resources)

--- Project2/server/src/main/java/com/TheEnd/www/customer/ReqBody.java
+++
@@ -1,6 +0,0 @@
-package com.TheEnd.www.customer;
-
-import java.util.ArrayList;
-
-public record ReqBody(String type, ArrayList content) {
-}
--- Project2/server/src/main/java/com/TheEnd/www/db/DBImplement.java
+++ Project2/server/src/main/java/com/TheEnd/www/db/DBImplement.java
@@ -4,8 +4,6 @@ import com.TheEnd.www.db.requestTypes.PostOpType;
 import com.TheEnd.www.db.requestTypes.SearchOpType;
 import com.TheEnd.www.db.requestTypes.UserOpType;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -17,7 +15,7 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @SpringBootApplication
-public class DBImplement implements CommandLineRunner, DBOperators {
+public class DBImplement implements  DBOperators {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
@@ -40,15 +38,15 @@ public class DBImplement implements CommandLineRunner, DBOperators {
         });
     }
 
-    public static void main(String[] args) {
-        SpringApplication.run(DBImplement.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println("Hello, this is a test for Spring Boot with JDBC");
-        jdbcTemplate.update("insert into rubbish(name,num) values('fuck',3)");
-    }
+//    public static void main(String[] args) {
+//        SpringApplication.run(DBImplement.class, args);
+//    }
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        System.out.println("Hello, this is a test for Spring Boot with JDBC");
+//        jdbcTemplate.update("insert into rubbish(name,num) values('fuck',3)");
+//    }
 
     @Override
     public ArrayList dealUser(UserOpType t, ArrayList content) {
